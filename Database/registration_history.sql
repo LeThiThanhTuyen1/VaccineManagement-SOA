4790047e92de729cae6dbbfeb049cc85cbb4e97e
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2025-01-15 15:11:46 +0700

    fix database

--- Database/registration_history.sql
+++ Database/registration_history.sql
@@ -11,17 +11,17 @@ CREATE TABLE registrations (
     status VARCHAR(10) CHECK (status IN ('PENDING', 'COMPLETED', 'CANCELLED')) DEFAULT 'PENDING'
 );
 
-CREATE TABLE vaccination_history (
-    id BIGINT PRIMARY KEY IDENTITY(1,1),
-    citizen_id BIGINT,
-    vaccine_id BIGINT,
-    vaccination_date DATE,
-    status VARCHAR(10) CHECK (status IN ('COMPLETED', 'MISSED')) DEFAULT 'COMPLETED'
-);
+-- CREATE TABLE vaccination_history (
+--     id BIGINT PRIMARY KEY IDENTITY(1,1),
+--     citizen_id BIGINT,
+--     vaccine_id BIGINT,
+--     vaccination_date DATE,
+--     status VARCHAR(10) CHECK (status IN ('COMPLETED', 'MISSED')) DEFAULT 'COMPLETED'
+-- );
 
 -- Thêm dữ liệu đăng ký tiêm
-INSERT INTO registrations (citizen_id, vaccine_id, registration_date, location, status) VALUES
-(1, 1, '2024-12-01', N'Trung tâm y tế Ba Đình', 'PENDING');
+-- INSERT INTO registrations (citizen_id, vaccine_id, registration_date, location, status) VALUES
+-- (1, 1, '2024-12-01', N'Trung tâm y tế Ba Đình', 'PENDING');
 
 -- Thêm lịch sử tiêm
 INSERT INTO vaccination_history (citizen_id, vaccine_id, vaccination_date, status) VALUES
